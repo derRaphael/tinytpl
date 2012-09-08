c6889b5dfe1e64bf482459ffe2c1bd5df98f8b1e
derRaphael <software@itholic.org>
2012-09-09 01:46:09 +0200

    Added a better description for tiny's error handling

--- tpl/config_tpl/error_demo.php
+++ tpl/config_tpl/error_demo.php
@@ -28,12 +28,58 @@
     $this->data["HEADER"] = "Error testing";
 ?>
 <h2>Error testing</h2>
-<p>This pages features a linklist to provoke special php errortypes to demonstrate tiny's errorhandling.</p>
+<p>
+    This pages features a linklist to provoke special php errortypes to demonstrate tiny's
+    errorhandling.
+</p>
+<p>
+    As long as the app is in the "dev" state, tinyTpl is happy to help you with as many
+    detailed message as possible. Being put into "stable" mode, tiny avoids exposing any
+    messages at all. Having php configured to log message in the server's "error"-file
+    is a good idea.
+</p>
 <ul>
-    <li><a href="/tinyAdmin/error_demo/exception_error">Throw an Exception</a></li>
-    <li><a href="/tinyAdmin/error_demo/code_error">Uninitialised variable usage</a></li>
-    <li><a href="/tinyAdmin/error_demo/syntax_error">Syntax error (missing semicolon)</a></li>
-    <li><a href="/tinyAdmin/error_demo/notfound_error">Invoking nonexistant template</a></li>
-    <li><a href="/tinyAdmin/error_demo/real_notfound_error">Linking nonexistant page</a></li>
+    <li>
+        <h3><a href="/tinyAdmin/error_demo/exception_error">Throw an Exception</a></h3>
+        <p>
+            Exceptions can be thrown in either your own routines, or by 3rd party libraries
+            or even by php. However, catching these is generally a good idea.
+            This page shows a custom trace, so you may find the lines throwing exceptions.
+            Any given paths apart from tinyTpl's own root folder will be masked,
+            so any internal server file structures won't be exposed.
+        </p>
+    </li>
+    <li>
+        <h3><a href="/tinyAdmin/error_demo/code_error">Uninitialised variable usage</a></h3>
+        <p>
+            Accessing variables, without having them defined somewhere is a bad idea, this
+            page illustrates these kind of errors.
+        </p>
+    </li>
+    <li>
+        <h3><a href="/tinyAdmin/error_demo/syntax_error">Syntax error</a></h3>
+        <p>
+            Somethign as simple as a missing semicolon may break your page's functionality.
+            This is a so called 'syntax error' is in php terms a shutdown, meaning the
+            interpreter will stop further processing and die with an error message.
+            Tiny catches this case and shows a somewhat more userfriendly page.
+        </p>
+    </li>
+    <li>
+        <h3><a href="/tinyAdmin/error_demo/notfound_error">Invoking nonexistant template</a></h3>
+        <p>
+            Non existant inline templates won't be rendered and wont show an error page. So by
+            clicking above's link, you will see a blank page by intention - this is usefull when
+            you made unintentionally a typo, where a template should have been accessed and
+            it cannot be rendered. So instead of showing an error page, you will simply see nothing.
+        </p>
+    </li>
+    <li>
+        <h3><a href="/tinyAdmin/error_demo/real_notfound_error">Linking nonexistant page</a></h3>
+        <p>
+            You will be forwarded to a guaranteed non existant page to provoke a 404 Document
+            not found Error
+        </p>
+    </li>
 </ul>
 <?php endif; ?>
