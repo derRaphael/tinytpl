c1d623eca1b13de30c8192b447fea6fa725c6e07
derRaphael <software@itholic.org>
2013-03-11 17:28:25 +0100

    cleaned cache

--- cache/4600d24324fdae7261ff53630a21792714bab374.js
+++
@@ -1,3 +0,0 @@
-/* u:639-c:415-d:224 (35.05%) */
-
-if(!(window.console&&console.log)){(function(){var noop=function(){};var methods=['assert','clear','count','debug','dir','dirxml','error','exception','group','groupCollapsed','groupEnd','info','log','markTimeline','profile','profileEnd','markTimeline','table','time','timeEnd','timeStamp','trace','warn'];var length=methods.length;var console=window.console={};while(length--){console[methods[length]]=noop;}}());}
--- cache/976cdfc4868c7ca62e2aa44fd7811ac91d4f5270.js
+++
@@ -1,2 +0,0 @@
-/* u:36-c:36-d:0 (0%) */
-document.location.href="/tinyAdmin";
